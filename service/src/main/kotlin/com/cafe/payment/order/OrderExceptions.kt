506b8519db48d355cb25a93613b1799e082b0138
yae <yaeyae9919@gmail.com>
2025-05-25 20:25:20 +0900

    refactor: Refactors order exceptions

--- service/src/main/kotlin/com/cafe/payment/order/OrderExceptions.kt
+++ service/src/main/kotlin/com/cafe/payment/order/OrderExceptions.kt
@@ -1,27 +1,62 @@
 package com.cafe.payment.order
 
-// 주문 상태 관련 예외
-class OrderConfirmationStatusException(
+import com.cafe.payment.library.HttpStatusCode
+import com.cafe.payment.library.exception.CustomException
+
+sealed class OrderException(
+    statusCode: HttpStatusCode,
+    errorCode: String,
     message: String,
     cause: Throwable? = null,
-) : RuntimeException(message, cause) {
+) : CustomException(statusCode, errorCode, message, cause)
+
+// 주문 상태 관련 예외
+class OrderConfirmationStatusException(
+    errorCode: String,
+    message: String,
+) : OrderException(HttpStatusCode.BAD_REQUEST, errorCode, message, null) {
     companion object {
-        fun alreadyCanceled() = OrderConfirmationStatusException("주문이 이미 취소되었어요.")
+        fun alreadyCanceled(): OrderConfirmationStatusException {
+            return OrderConfirmationStatusException(
+                errorCode = "ORDER_STATUS_001",
+                message = "주문이 이미 취소되었어요.",
+            )
+        }
 
-        fun alreadyPaid() = OrderConfirmationStatusException("주문이 이미 완료되었어요.")
+        fun alreadyPaid(): OrderConfirmationStatusException {
+            return OrderConfirmationStatusException(
+                errorCode = "ORDER_STATUS_002",
+                message = "주문이 이미 완료되었어요.",
+            )
+        }
 
-        fun notPaid() = OrderConfirmationStatusException("아직 주문이 완료되지 않았어요.")
+        fun notPaid(): OrderConfirmationStatusException {
+            return OrderConfirmationStatusException(
+                errorCode = "ORDER_STATUS_003",
+                message = "아직 주문이 완료되지 않았어요.",
+            )
+        }
     }
 }
 
 // 주문 데이터 유효성 관련 예외
 class InvalidOrderItemException(
+    errorCode: String,
     message: String,
-    cause: Throwable? = null,
-) : RuntimeException(message, cause) {
+) : OrderException(HttpStatusCode.BAD_REQUEST, errorCode, message, null) {
     companion object {
-        fun invalidQuantity() = InvalidOrderItemException("주문 수량은 1개 이상이어야 합니다.")
+        fun invalidQuantity(): InvalidOrderItemException {
+            return InvalidOrderItemException(
+                errorCode = "ORDER_ITEM_001",
+                message = "주문 수량은 1개 이상이어야 합니다.",
+            )
+        }
 
-        fun invalidOrderItems() = InvalidOrderItemException("주문 상품이 비어있습니다.")
+        fun invalidOrderItems(): InvalidOrderItemException {
+            return InvalidOrderItemException(
+                errorCode = "ORDER_ITEM_002",
+                message = "주문 상품이 비어있습니다.",
+            )
+        }
     }
 }
